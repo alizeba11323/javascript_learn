8cda8bd6b0cc90df1f9646c2a97c33dae1ff841d
alizeba11323 <alizeba11323@gmail.com>
2025-03-04 16:00:45 +0530

    object javascript

--- index.js
+++ index.js
@@ -51,19 +51,128 @@
 
 // first class function , function class citizen
 
-function showSum(sum) {
-  return function newFunc(a, b) {
-    return sum(a, b);
-  };
-}
+// function showSum(sum) {
+//   return function newFunc(a, b) {
+//     return sum(a, b);
+//   };
+// }
 
-// const func1 = showSum(sum);
-// const func2 = func1(1, 2);
-// console.log(func1);
-// console.log(func2);
+// // const func1 = showSum(sum);
+// // const func2 = func1(1, 2);
+// // console.log(func1);
+// // console.log(func2);
 
-console.log(showSum(sum)(1, 2));
+// console.log(showSum(sum)(1, 2));
 
-function sum(a, b) {
-  return a + b;
-}
+// function sum(a, b) {
+//   return a + b;
+// }
+
+//arrow function()
+//syntax
+//3. hoisting
+//4. this
+//
+
+// var sum = (...args) => {
+//   console.log(arguments);
+// };
+
+// sum(1, 2, 3, 4, 5, 6);
+
+// const name = "name someone";
+// const age = 25;
+// const isMarried = true;
+// //object with name of Person
+
+// //creation of object
+// const person = {
+//   name: "Name Someone",
+//   age: 25,
+//   isMarried: true,
+//   "full name": "User FullName",
+//   address: {
+//     city: "City One",
+//     street: "Street One",
+//     "pin code": 821106,
+//   },
+// };
+// //nested object
+// // 2 methods
+// //1 . dot notation
+// //2 [] notation
+// // access object property
+// console.log(person.name);
+// console.log(person["name"]);
+// console.log(person.age);
+// console.log(person["full name"]);
+
+// console.log(person.address.city);
+// console.log(person.address["pincode"]);
+
+// ///update something inside the object
+
+// person.age = 27;
+
+// person.name = "Name Updated!!";
+
+// console.log(person);
+
+// // delete property from object
+
+// delete person.age;
+
+// console.log(person);
+
+// person.address.street = "New Street";
+// person["full name"] = "Updated Full name";
+
+// person.address["pin code"] = 821008;
+
+// console.log(person);
+
+// delete person.address["pin code"];
+
+// console.log(person);
+
+const fullname = "Fullname";
+const person = {
+  name: "Name One",
+  [fullname]: "FullName Value",
+  age: 27,
+  isMarried: true,
+  address: {
+    city: "City One",
+    street: "Street One",
+  },
+};
+
+console.log(person);
+
+//function called Object.keys()
+
+// const keys = Object.keys(person); //array of values,keys
+// const values = Object.values(person);
+// const allvalue = Object.entries(person); //keys and value in the form of array [[name,"Name One"],["age",27],["isMarried",true]]
+// console.log(keys);
+// console.log(values);
+// console.log(allvalue);
+
+//object destructuring
+
+// const {
+//   age,
+//   address: { city },
+// } = person;
+// console.log(age);
+
+const {
+  address: { city, street },
+} = person;
+
+console.log(street);
+// function hasOwnProperty()
+// in operator
+console.log(person.hasOwnProperty("fullname"));
+console.log("name" in person);
+console.log("fullname" in person);
